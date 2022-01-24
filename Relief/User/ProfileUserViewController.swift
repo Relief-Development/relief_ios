41ce0f95c56b78dbc0f4c2b87ada1b9449d4339c
alejandro botana <abotanam@gmail.com>
2022-01-24 18:54:03 +0100

    Profile User

---
+++ Relief/User/ProfileUserViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileUserViewController.swift
+//  Relief
+//
+//  Created by Alejandro Botana on 24/1/22.
+//
+
+import UIKit
+
+class ProfileUserViewController: UIViewController, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+   
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
