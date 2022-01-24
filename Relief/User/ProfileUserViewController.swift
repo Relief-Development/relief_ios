e01c668cd51389ef5962804375705f85d7252b8a
alejandro botana <abotanam@gmail.com>
2022-01-24 20:55:07 +0100

    Union 2

--- Relief/User/ProfileUserViewController.swift
+++ Relief/User/ProfileUserViewController.swift
@@ -7,9 +7,16 @@
 
 import UIKit
 
-class ProfileUserViewController: UIViewController, UITableViewDelegate{
+class ProfileUserViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProfileUserCellIdentifier", for: indexPath)
+        return cell
     }
     
    
