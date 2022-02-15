0c2e6b57ec33f3e9270c3a8a6d61c58a2d697788
alejandro botana <abotanam@gmail.com>
2022-02-15 17:09:27 +0100

    Fix actionsheet camera

--- Relief/User/EditUserProfileVC.swift
+++ Relief/User/EditUserProfileVC.swift
@@ -6,8 +6,13 @@
 //
 
 import UIKit
+import AVFoundation
 
-class EditUserProfileVC: UIViewController{
+class EditUserProfileVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate{
+    
+    @IBOutlet var imageProfile: UIImageView!
+    let picker = UIImagePickerController()
+    
     
     
     override func viewDidLoad() {
@@ -17,5 +22,45 @@ class EditUserProfileVC: UIViewController{
     @IBAction func buttonToProfileTapped(){
         self.dismiss(animated: true, completion: nil)
         
+    }
+    @IBAction func galleryTapped(){
+        
+    }
+    @IBAction func buttonImageTapped(){
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alertController.view.tintColor = UIColor(named: "user")
+
+        let camara = UIAlertAction(title: "Hacer foto", style: .default, handler: {(action) in
+            self.picker.sourceType = .camera
+            self.picker.cameraCaptureMode = .photo
+            self.picker.delegate = self
+            self.present(self.picker, animated: true)
+        })
+        let galeria = UIAlertAction(title: "Seleccionar foto", style: .default, handler: {(action) in
+            self.picker.sourceType = .photoLibrary
+            self.picker.delegate = self
+            self.present(self.picker, animated: true)
+        })
+        let cancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: {(action) in
+            
+        })
+        alertController.addAction(camara)
+        alertController.addAction(galeria)
+        alertController.addAction(cancelar)
+
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else{
+            return
+        }
+        imageProfile.image = image
     }
 }
